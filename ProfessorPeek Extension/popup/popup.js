da9c8f5ee0b9cec8feed90727136436bc3efe124
Ronald Jabouin <rj336@cornell.edu>
2024-04-28 13:35:43 -0400

    made basic popup

---
+++ ProfessorPeek Extension/popup/popup.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('button');
+button.addEventListener('click', async () => {
+    document.addEventListener('DOMContentLoaded', function() {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        });
+      });      
+});

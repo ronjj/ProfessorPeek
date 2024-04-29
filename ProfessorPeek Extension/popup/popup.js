e06c9a7f42f778d229f3439c1244bf6a7824bc4a
Ronald Jabouin <rj336@cornell.edu>
2024-04-29 14:59:34 -0400

    changed styling of popup

--- ProfessorPeek Extension/popup/popup.js
+++
@@ -1,9 +0,0 @@
-// const button = document.querySelector('button');
-// button.addEventListener('click', async () => {
-//     document.addEventListener('DOMContentLoaded', function() {
-//         window.scrollTo({
-//           top: 0,
-//           behavior: 'smooth'
-//         });
-//       });      
-// });

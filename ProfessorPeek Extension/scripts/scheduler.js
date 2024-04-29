99b4ee3261e3575317ba096f8e39ca750df62b69
Ronald Jabouin <rj336@cornell.edu>
2024-04-28 21:42:48 -0400

    removed code duplication of CUReviews function on scheduler page

--- ProfessorPeek Extension/scripts/scheduler.js
+++ ProfessorPeek Extension/scripts/scheduler.js
@@ -1,41 +1,4 @@
-async function getCUReviewsInfo(subject, courseNumber) {
-    const response = await fetch(`https://www.cureviews.org/api/getCourseByInfo`, {
-        method: "POST",
-        body: JSON.stringify({
-            number: courseNumber,
-            subject: subject.toLowerCase()
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    })
-  
-    const data = await response.json();
-    const classInfo = data.result; // Access the "result" property
-    const classDifficulty = classInfo.classDifficulty;
-    const classRating = classInfo.classRating;
-    const classWorkload = classInfo.classWorkload;
-    const classId = classInfo._id;
-  
-    const response_two = await fetch(`https://www.cureviews.org/api/getReviewsByCourseId`, {
-        method: "POST",
-        body: JSON.stringify({
-          courseId: classId,
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    })
-  
-    const data_two = await response_two.json();
-    const num_reviews = data_two.result.length;
-    const review_preview = data_two.result.slice(0, 2);
-  
-    return [classDifficulty, classRating, classWorkload, num_reviews, review_preview];
-  }
-
-
-  function onDOMReady(fn) {
+function onDOMReady(fn) {
     if (document.readyState === 'complete' || document.readyState === 'interactive') {
         console.log('Document already loaded, running function immediately.');
         fn();

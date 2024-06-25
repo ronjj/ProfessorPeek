547bf54805a7e522f2225bc8fbec8f5b53354d23
Ronald Jabouin <rj336@cornell.edu>
2024-06-24 21:24:18 -0700

    made function to get course subject code and short description

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import os
 # Scraper For Getting Courses From Cornell Course Catalog
 
 # Get all subject codes from main course roster page
@@ -16,7 +17,7 @@ def getSubjectCodes():
 
     return a_tags_text
 
-def getCoursesForSubject(subject):
+def getCoursesForSubject(subject, create_text_file=False):
     URL = f"https://classes.cornell.edu/browse/roster/FA24/subject/{subject}"
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
@@ -25,13 +26,35 @@ def getCoursesForSubject(subject):
     nodes = soup.find_all('div', class_='node')
     for node in nodes:
         try:
-            title = node.find('div', class_='title-subjectcode').text.strip()
+            course_code = node.find('div', class_='title-subjectcode').text.strip()
             description = node.find('p', class_='course-descr').text.strip()
-            classes.append({'title': title, 'description': description})
+            classes.append({'title': course_code, 'description': description})
         except:
             continue
 
-    return classes
+    if create_text_file:
+        # Create the 'data' directory if it doesn't exist
+        if not os.path.exists('data'):
+            os.makedirs('data')
 
-print(getCoursesForSubject('CS'))
+        # Write the output to a file in the 'data' directory
+        with open(os.path.join('data', f"{subject}.txt"), "w") as file:
+            for cls in classes:
+                file.write(f"Course Code: {cls['course_code']}\n")
+                file.write(f"Description: {cls['description']}\n")
+                file.write("\n")
+    
+    
+        print("Created text file for subject: ", subject)
+    else:
+        print(classes)
+
+print(getCoursesForSubject("AEM", create_text_file=False))
+# if __name__ == "__main__":
+#     subjectCodes = getSubjectCodes()
+#     for subject in subjectCodes:
+#         getCoursesForSubject(subject)
+#     print("Finished scraping all subjects")
+
+
 

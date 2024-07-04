ccd9859689fe86fd2c39dc18322995d011edf168
Ronald Jabouin <rj336@cornell.edu>
2024-07-04 14:59:44 -0400

    add outer try catch block to get CUReviewsinfo function

--- professor-peek/contents/main.ts
+++ professor-peek/contents/main.ts
@@ -118,50 +118,55 @@ export async function getRateMyProfessorScore(professorLastName, professorFirstN
   
   // Function To Get CUReviews Information For A Course
 export async function getCUReviewsInfo(subject, courseNumber) {
-    const response = await fetch(`https://www.cureviews.org/api/getCourseByInfo`, {
-        method: "POST",
-        body: JSON.stringify({
-            number: courseNumber,
-            subject: subject.toLowerCase()
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
+    try {
+        const response = await fetch(`https://www.cureviews.org/api/getCourseByInfo`, {
+            method: "POST",
+            body: JSON.stringify({
+                number: courseNumber,
+                subject: subject.toLowerCase()
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+      
+        const data = await response.json();
+        if (data.result === null || data.result === undefined) {
+            console.log(`No CUReviews For: ${subject} ${courseNumber}`);
+            throw new Error(`No CUReviews For: ${subject} ${courseNumber}`);
+        } else {
+            try {
+                const classInfo = data.result; // Access the "result" property
+                const classDifficulty = classInfo.classDifficulty;
+                const classRating = classInfo.classRating;
+                const classWorkload = classInfo.classWorkload;
+                const classId = classInfo._id;
+              
+                const response_two = await fetch(`https://www.cureviews.org/api/getReviewsByCourseId`, {
+                    method: "POST",
+                    body: JSON.stringify({
+                      courseId: classId,
+                    }),
+                    headers: {
+                        "Content-type": "application/json; charset=UTF-8"
+                    }
+                })
+              
+                const data_two = await response_two.json();
+                const num_reviews = data_two.result.length;
+                const review_preview = data_two.result.slice(0, 2);
+              
+                return [classDifficulty, classRating, classWorkload, num_reviews, review_preview];
+            } catch (error) {
+                console.error("getCUReviewsInfo: Error fetching CUReviews data:", error.message);
+                return [0, 0, 0, 0, []];
+            }
         }
-    })
-  
-    const data = await response.json();
-    if (data.result === null || data.result === undefined) {
-        console.log(`No CUReviews For: ${subject} ${courseNumber}`);
-        throw new Error(`No CUReviews For: ${subject} ${courseNumber}`);
-    } else {
-        try {
-            const classInfo = data.result; // Access the "result" property
-            const classDifficulty = classInfo.classDifficulty;
-            const classRating = classInfo.classRating;
-            const classWorkload = classInfo.classWorkload;
-            const classId = classInfo._id;
-          
-            const response_two = await fetch(`https://www.cureviews.org/api/getReviewsByCourseId`, {
-                method: "POST",
-                body: JSON.stringify({
-                  courseId: classId,
-                }),
-                headers: {
-                    "Content-type": "application/json; charset=UTF-8"
-                }
-            })
-          
-            const data_two = await response_two.json();
-            const num_reviews = data_two.result.length;
-            const review_preview = data_two.result.slice(0, 2);
-          
-            return [classDifficulty, classRating, classWorkload, num_reviews, review_preview];
-        } catch (error) {
-            console.error("getCUReviewsInfo: Error fetching CUReviews data:", error.message);
-            return [0, 0, 0, 0, []];
-        }
-    }
+    } catch (error) {
+        console.error("getCUReviewsInfo: Error fetching CUReviews data:", error.message);
+        return [0, 0, 0, 0, []];
   }
+}
   
   // Get Course Names, Get CUReviews Info, Add Data to Page
 export async function processCourseNames() {
